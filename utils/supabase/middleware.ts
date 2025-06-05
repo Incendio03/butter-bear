381a238b8cfb58478cd108b356c40e7c85c591f4
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-06-06 03:56:59 +0800

    Setup simple auth
    This commit includes the following changes
    - Installation of new components from shadcn (login-form)
    - Modified supabase middleware for auth actions (What page to redirect if authenticated or not)
    - Setting up login page (Login function only)

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,11 +40,13 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    request.nextUrl.pathname !== '/signup' &&
+    request.nextUrl.pathname !== '/' 
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
