83a43a498fafd2dc878be8221863d13fe1091a00
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-06-06 18:23:07 +0800

    Created Signup form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { SignupForm } from '@/components/signup-form'
 
 export default function Page() {
-    
-
     return (
-        <>
-        <h1>Signup Page</h1>
-        </>
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-md">
+                <SignupForm />
+            </div>
+        </div>
     )
 }
