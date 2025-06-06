f82a89879cb4528aed3137befd9e046fe5434a72
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-06-06 17:35:29 +0800

    Update middleware
    - This update includes role checking for accessing role specific routes

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -37,6 +37,36 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  const isAdminRoute = request.nextUrl.pathname.startsWith('/admin')
+  const isVendorRoute = request.nextUrl.pathname.startsWith('/vendor')
+
+  if (user && (isAdminRoute || isVendorRoute)) {
+    //Get user profile to check role
+    const { data: profile } = await supabase
+      .from('profiles')
+      .select('role')
+      .eq('id', user.id)
+      .single()
+
+    if(profile) {
+      //Check admin access
+      if (isAdminRoute && !['admin', 'super_admin'].includes(profile.role)) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/'
+        return NextResponse.redirect(url)
+      }
+
+      //Check vendor access
+      if (isVendorRoute && !['vendor', 'admin', 'super_admin'].includes(profile.role)) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/'
+        return NextResponse.redirect(url)
+      }
+    }
+  }
+
+
+  //Regular auth checks
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
